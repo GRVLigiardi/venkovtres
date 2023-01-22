0516fb72600f5c463a5bbcb021fe6eed710385b1
GRVLigiardi <111814761+GRVLigiardi@users.noreply.github.com>
2023-01-22 13:24:55 -0300

    Premier commit

---
+++ assets/js/productos.js
@@ -0,0 +1,18 @@
+// var triggers = document.querySelectorAll(".lightbox-trigger");
+// var lightbox = document.querySelector(".lightbox");
+// var close = document.querySelector(".lightbox-close");
+
+// triggers.forEach(function(trigger) {
+//   trigger.addEventListener("click", function() {
+//     lightboxImg.src = this.dataset.img;
+//     lightbox.style.display = "block";
+//   });
+// });
+
+// close.addEventListener("click", function() {
+//   lightbox.style.display = "none";
+// });
+
+
+  
+
